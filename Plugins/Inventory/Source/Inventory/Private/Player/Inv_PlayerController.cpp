b1b7987835f7c989b8e0d761b7d11ffa5e4a297d
Zhikun0201 <37325449+MorimiIchi@users.noreply.github.com>
2025-06-18 01:43:03 +0800

    7. Primary Interact Action

--- Plugins/Inventory/Source/Inventory/Private/Player/Inv_PlayerController.cpp
+++ Plugins/Inventory/Source/Inventory/Private/Player/Inv_PlayerController.cpp
@@ -2,11 +2,39 @@
 
 
 #include "Player/Inv_PlayerController.h"
-#include "Inventory.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
 
 void AInv_PlayerController::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogInventory, Log, TEXT("BeginPlay for PlayerController"))
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(
+		GetLocalPlayer());
+	if (IsValid(Subsystem))
+	{
+		for (UInputMappingContext* IMC : DefaultIMCs)
+		{
+			if (IsValid(IMC))
+			{
+				Subsystem->AddMappingContext(IMC, 0);
+			}
+		}
+	}
+}
+
+void AInv_PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	EnhancedInputComponent->BindAction(PrimaryInteractAction, ETriggerEvent::Started, this,
+	                                   &AInv_PlayerController::PrimaryInteract);
+}
+
+void AInv_PlayerController::PrimaryInteract()
+{
+	UE_LOG(LogTemp, Log, TEXT("Primary Interact"))
 }
--- Plugins/Inventory/Source/Inventory/Public/Player/Inv_PlayerController.h
+++ Plugins/Inventory/Source/Inventory/Public/Player/Inv_PlayerController.h
@@ -1,10 +1,13 @@
-﻿﻿// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "Inv_PlayerController.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
 
 /**
  * 
@@ -13,6 +16,17 @@ UCLASS()
 class INVENTORY_API AInv_PlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
 protected:
 	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+
+private:
+	void PrimaryInteract();
+
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
+	TArray<TObjectPtr<UInputMappingContext>> DefaultIMCs;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
+	TObjectPtr<UInputAction> PrimaryInteractAction;
 };
