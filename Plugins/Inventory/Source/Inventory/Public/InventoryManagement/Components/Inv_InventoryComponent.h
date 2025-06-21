c898b5b51083bec5ac65b935d2d86a373423f2f1
Zhikun0201 <37325449+MorimiIchi@users.noreply.github.com>
2025-06-21 15:32:23 +0800

    14. Inventory Component

---
+++ Plugins/Inventory/Source/Inventory/Public/InventoryManagement/Components/Inv_InventoryComponent.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Inv_InventoryComponent.generated.h"
+
+
+class UInv_InventoryBase;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent), Blueprintable)
+class INVENTORY_API UInv_InventoryComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UInv_InventoryComponent();
+
+	void ToggleInventory();
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	void ConstructInventory();
+
+	TWeakObjectPtr<APlayerController> OwningController;
+
+	UPROPERTY()
+	TObjectPtr<UInv_InventoryBase> InventoryMenu;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
+	TSubclassOf<UInv_InventoryBase> InventoryMenuClass;
+};
---
+++ Plugins/Inventory/Source/Inventory/Public/Widgets/Inventory/Spatial/Inv_SpacialInventory.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/Inventory/InventoryBase/Inv_InventoryBase.h"
+#include "Inv_SpacialInventory.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INVENTORY_API UInv_SpacialInventory : public UInv_InventoryBase
+{
+	GENERATED_BODY()
+};
